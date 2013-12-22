fbe4c5e779e5a5aa51a10c66cbf4f656ee3cd1b2
Vadym Volokitin <seagsm@mail.ru>
2013-12-22 17:47:04 +0100

    added src folder.

---
+++ src/inc/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H 1
+
+
+
+#include "stm32f10x.h"
+
+/* Checking for libs parameters. */
+/* void assert_failed(uint8_t* file, uint32_t line); */
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,38 @@
+
+#include "main.h"
+
+
+#include <stm32f10x.h>
+#include <stm32f10x_gpio.h>
+#include <stm32f10x_rcc.h>
+#include <stm32f10x_tim.h>
+#include <misc.h>
+
+int main( void)
+{
+
+}
+
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+uint32_t assert_failed(uint8_t* file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1U)
+  {
+    return(1U);
+  }
+
+}
+#endif
+
