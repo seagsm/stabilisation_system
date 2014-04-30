7efe9eb772eec9714bd1f6714bd35f6240d8dec0
Vadym Volokitin Office <seagsm_trash@mail.ru>
2014-04-30 14:45:36 +0200

    Added some explanation to api_pid.c

--- src/api/api_pid.c
+++ src/api/api_pid.c
@@ -95,10 +95,10 @@ static void api_pid_update_PDF(PID_element *current_pid, int32_t i32_set_point, 
     */
 
     /* Level error calculation. */
-    i32_position_error = constrain_i32(2 * i32_set_point - i32_angle_position, -MAX_ANGLE_INCLINATION, MAX_ANGLE_INCLINATION);
+    i32_position_error = constrain_i32(2 * i32_set_point - i32_angle_position, -MAX_ANGLE_INCLINATION, MAX_ANGLE_INCLINATION); /* -450 to +450 () in 0.1 degree. */
 
    /* Level proportional stabilisation. */
-    i32_p_term = (current_pid->i32_p_gain * i32_position_error) /100;
+    i32_p_term = (current_pid->i32_p_gain * i32_position_error) /100;  /* -450 +450  * (p_gain = 125 )  / 100 =  +- 562 */
 
    /*
     * p_dyn_gain->p roll or pitch arround 70
@@ -108,16 +108,19 @@ static void api_pid_update_PDF(PID_element *current_pid, int32_t i32_set_point, 
     * 0.244/0.0125 ~ 19
     * So 19 is fitting coefficient
     */
-
-
-
-
+    /*
+        In our case i bit is 0.0175 degree/sec
+        So , K = 0.244/0.0175 = 14
+        for RAW input.
+    */
 
     /* Angle speed proportional stabilisation */
-    i32_p_term = i32_p_term - (i32_angle_speed * current_pid->i32_p_dyn_gain)/10;
+     
+    i32_p_term = i32_p_term - (i32_angle_speed * current_pid->i32_p_dyn_gain)/80/14; /* +- 5000 * 40 /80/14 = +-178 */  
 
     /* Integration. */
     current_pid->i32_i_state = constrain_i32(current_pid->i32_i_state + i32_position_error, current_pid->i32_i_min, current_pid->i32_i_max);
+    
     /* Calculation i_term. */
     i32_i_term = (current_pid->i32_i_state * current_pid->i32_i_gain)/4096; /* pid->i_gain arround 13,so resul arround +- 30. */
 
@@ -132,7 +135,7 @@ static void api_pid_update_PDF(PID_element *current_pid, int32_t i32_set_point, 
     current_pid->i32_d_1_state = current_pid->i32_d_0_state;
     current_pid->i32_d_0_state = i32_d_state;
 
-    i32_d_term = (i32_d_summ * current_pid->i32_d_gain)/2048 ;/* //32// pid->d_gain arround 40 , so pid->d_gain/32 arround 1~2 */
+    i32_d_term = (i32_d_summ * current_pid->i32_d_gain)/120 ;/* 100deg/sec -> 1000 * (pid->d_gain = 60) / 120, so arround 50 */
 
     /* Save result of PID calculation back to structure. */
     current_pid->i32_pid_output = i32_p_term + i32_i_term - i32_d_term;
@@ -151,17 +154,17 @@ void api_pid_update_frame(void)
     /* Calculation of Pitch PDF frame. */
 
     /* Convert channel value to int32. */
-    i32_rc_chanel_value = (int32_t)bc_channel_value_structure.u16_channel_2_value;
+    i32_rc_chanel_value = (int32_t)bc_channel_value_structure.u16_channel_2_value;      /* value between 1000 - 2000  */
 
     /* Get deviation from ZERO value. */
-    i32_rc_chanel_value = i32_rc_chanel_value - BOARD_PPM_ZERO_VALUE;
+    i32_rc_chanel_value = i32_rc_chanel_value - BOARD_PPM_ZERO_VALUE;  /* value between -500 + 500  */
 
     /* Convert body inclination from degree to 0.1 degree. ( 10degree = 100(0.1degree))*/
-    f_body_angle        = fl_api_body_angle_wind_angles[Pitch] * 10.0f;
-    i32_body_angle      = (int32_t)f_body_angle;
+    f_body_angle        = fl_api_body_angle_wind_angles[Pitch] * 10.0f; 
+    i32_body_angle      = (int32_t)f_body_angle;        /* value between -1800 + 1800 */
 
     /* Convert body angle speed from degree/sec to 0.1 degree/sec. */
-    f_body_angle_speed  = b_float3d_api_data_norm_gyro_data.fl_Y * 10.0f;
+    f_body_angle_speed  = b_float3d_api_data_norm_gyro_data.fl_Y * 10.0f; /* value between -5000 + 5000  , it could be used RAW data from Gyro. */
     i32_body_angle_speed= (int32_t)f_body_angle_speed;
 
     /* update Pitch frame. */
