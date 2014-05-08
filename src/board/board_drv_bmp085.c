97a6cbcc27e3e513cba36fa313babeb5a509398a
Vadym Volokitin <seagsm@mail.ru>
2014-05-08 22:36:15 +0200

    Added baro read calibration registers.

--- src/board/board_drv_bmp085.c
+++ src/board/board_drv_bmp085.c
@@ -3,15 +3,74 @@
 
 #include "board_drv_bmp085.h"
 
-
+static BMP85_STATE  bs_bmp85_state;
 
 BOARD_ERROR board_drv_bmp085_init(void)
 {
     BOARD_ERROR be_result = BOARD_ERR_OK;
 
+    gv_board_sys_tick_delay(10U);
+/*
+    i2c_BMP085_readCalibration();
+    i2c_BMP085_UT_Start();
 
+    gv_board_sys_tick_delay(5U);
+
+    i2c_BMP085_UT_Read();
+
+
+*/
     return (be_result);
 }
+
+BOARD_ERROR board_drv_bmp085_callibration_read(void)
+{
+    BOARD_ERROR be_result = BOARD_ERR_OK;
+
+    uint8_t u8_callibration_data[BMP085_REG_SIZEOF];
+    gv_board_sys_tick_delay(10U);
+
+    /* Read calibration data from calibration registers. */
+    be_result = board_i2c_read( BMP085_ADDRESS, BMP085_REG_START_ADDRESS, BMP085_REG_SIZEOF, &u8_callibration_data[0]);
+
+    /* Setup data to registers structure. */
+    bs_bmp85_state.i16_ac1 = (( int16_t)u8_callibration_data[0]  * 256) + (( int16_t)u8_callibration_data[1]);
+    bs_bmp85_state.i16_ac2 = (( int16_t)u8_callibration_data[2]  * 256) + (( int16_t)u8_callibration_data[3]);
+    bs_bmp85_state.i16_ac3 = (( int16_t)u8_callibration_data[4]  * 256) + (( int16_t)u8_callibration_data[5]);
+    bs_bmp85_state.u16_ac4 = ((uint16_t)u8_callibration_data[6]  << 8 ) + ((uint16_t)u8_callibration_data[7]);
+    bs_bmp85_state.u16_ac5 = ((uint16_t)u8_callibration_data[8]  << 8 ) + ((uint16_t)u8_callibration_data[9]);
+    bs_bmp85_state.u16_ac6 = ((uint16_t)u8_callibration_data[10] << 8 ) + ((uint16_t)u8_callibration_data[11]);
+    bs_bmp85_state.i16_b1  = (( int16_t)u8_callibration_data[12] * 256) + (( int16_t)u8_callibration_data[13]);
+    bs_bmp85_state.i16_b2  = (( int16_t)u8_callibration_data[14] * 256) + (( int16_t)u8_callibration_data[15]);
+    bs_bmp85_state.i16_mb  = (( int16_t)u8_callibration_data[16] * 256) + (( int16_t)u8_callibration_data[17]);
+    bs_bmp85_state.i16_mc  = (( int16_t)u8_callibration_data[18] * 256) + (( int16_t)u8_callibration_data[19]);
+    bs_bmp85_state.i16_md  = (( int16_t)u8_callibration_data[20] * 256) + (( int16_t)u8_callibration_data[21]);
+
+    return(be_result);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 int32_t i32_board_drv_bmp085_calculate_pressure(void)
 {
--- src/board/board_drv_bmp085.h
+++ src/board/board_drv_bmp085.h
@@ -7,9 +7,49 @@
 #include "board_sys_tick.h"
 #include "board_i2c.h"
 
+#define BMP085_ADDRESS 0x77U
+#define BMP085_REG_START_ADDRESS 0xAAU
+#define BMP085_REG_SIZEOF 0x16U   /* 11 * 2 bytes. */
+
+typedef struct
+{
+  /* sensor registers from the BOSCH BMP085 datasheet */
+  int16_t  i16_ac1;
+  int16_t  i16_ac2;
+  int16_t  i16_ac3;
+  uint16_t u16_ac4;
+  uint16_t u16_ac5;
+  uint16_t u16_ac6;
+  int16_t  i16_b1;
+  int16_t  i16_b2;
+  int16_t  i16_mb;
+  int16_t  i16_mc;
+  int16_t  i16_md;
+/*
+  union {uint16_t val; uint8_t raw[2]; } ut; //uncompensated T
+  union {uint32_t val; uint8_t raw[4]; } up; //uncompensated P
+*/
+  uint8_t  u8_ut[2];
+  uint8_t  u8_up[4];
+  uint8_t  u8_state;
+  uint32_t u32_deadline;
+} BMP85_STATE;
+
+
+
+
+
 
 BOARD_ERROR board_drv_bmp085_init(void);
 int32_t i32_board_drv_bmp085_calculate_pressure(void);
+BOARD_ERROR board_drv_bmp085_callibration_read(void);
+
+
+
+
+
+
+
 
 
 #endif
