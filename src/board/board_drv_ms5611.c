667ba739d73707c665804c696e65a4532c4d3ec9
Vadym Volokitin <seagsm.trash@gmail.com>
2017-09-18 22:07:53 +0200

    Adding baro in progress

--- src/board/board_drv_ms5611.c
+++ src/board/board_drv_ms5611.c
@@ -7,8 +7,8 @@
 #include "board_drv_ms5611.h"
 
 
-static uint16_t ms5611_prom(int8_t coef_num);
-static int8_t ms5611_crc(uint16_t *prom);
+static uint16_t ms5611_prom(uint8_t u8_coef_num);
+static BOARD_ERROR ms5611_crc(uint16_t u16_prom[]);
 static uint32_t ms5611_read_adc(void);
 static void ms5611_start_ut(void);
 static void ms5611_get_ut(void);
@@ -27,6 +27,7 @@ BOARD_ERROR ms5611Detect(baro_t *baro)
     BOARD_ERROR be_result = BOARD_ERR_OK;
     uint8_t u8_sig;
     uint8_t u8_i;
+    uint16_t u16_value;
 
     gv_board_sys_tick_delay(10U); /* No idea how long the chip takes to power-up, but let's make it 10 mS */
 
@@ -40,14 +41,15 @@ BOARD_ERROR ms5611Detect(baro_t *baro)
         /* read all coefficients */
         for (u8_i = 0U; u8_i < PROM_NB; u8_i++)
         {
-            ms5611_c[u8_i] = ms5611_prom(u8_i);
+            u16_value = ms5611_prom(u8_i);
+            ms5611_c[u8_i] = u16_value;
         }
         /* check crc, bail out if wrong - we are probably talking to BMP085 w/o XCLR line! */
-        if (ms5611_crc(ms5611_c) == 0)
+        if (ms5611_crc(ms5611_c) == 0U)
         {
             /* TODO prom + CRC */
-            baro->ut_delay = 10000;
-            baro->up_delay = 10000;
+            baro->ut_delay = 10000U;
+            baro->up_delay = 10000U;
             baro->start_ut = ms5611_start_ut;
             baro->get_ut = ms5611_get_ut;
             baro->start_up = ms5611_start_up;
@@ -61,62 +63,80 @@ BOARD_ERROR ms5611Detect(baro_t *baro)
 BOARD_ERROR ms5611_reset(void)
 {
     BOARD_ERROR be_result = BOARD_ERR_OK;
-    be_result = board_i2c_write(MS5611_ADDR, CMD_RESET, 1);
-    gv_board_sys_tick_delay(2800);
+    be_result = board_i2c_write(MS5611_ADDR, CMD_RESET, 1U);
+    gv_board_sys_tick_delay(2800U);
     return (be_result);
 }
 
-static uint16_t ms5611_prom(int8_t i8_coef_num)
+static uint16_t ms5611_prom(uint8_t u8_coef_num)
 {
+    uint16_t u16_value = 0U;
     uint8_t u8_rxbuf[2] = { 0, 0 };
-    /* i2cRead(MS5611_ADDR, CMD_PROM_RD + i8_coef_num * 2, 2, u8_rxbuf); /* send PROM READ command */
-    be_result = board_i2c_read( MS5611_ADDR, CMD_PROM_RD + i8_coef_num * 2U, 2U, u8_rxbuf);
-    return ((u8_rxbuf[0] << 8 )| u8_rxbuf[1]);
+
+    /* i2cRead(MS5611_ADDR, CMD_PROM_RD + i8_coef_num * 2, 2, u8_rxbuf);  send PROM READ command */
+
+    board_i2c_read( MS5611_ADDR, CMD_PROM_RD + u8_coef_num * 2U, 2U, u8_rxbuf);
+
+    /* u16_value = ((u8_rxbuf[0] << 8 )| u8_rxbuf[1]); */
+     u16_value = (uint16_t)(u8_rxbuf[0]) << 8 ;
+     u16_value = u16_value | ((uint16_t)(u8_rxbuf[1]));
+
+    return (u16_value);
 }
 
-static BOARD_ERROR ms5611_crc(uint16_t *pu16_prom)
+static BOARD_ERROR ms5611_crc(uint16_t u16_prom[])
 {
     BOARD_ERROR be_result = BOARD_ERR_OK;
     int32_t     i32_i;
     int32_t     i32_j;
-    uint32_t    u32_res = 0;
-    uint8_t     u8_zero = 1;
+    uint16_t    u16_value;
+    uint32_t    u32_res = 0U;
+    uint8_t     u8_zero = 1U;
     uint8_t     u8_crc;
+    uint8_t     u8_value;
 
-    u8_crc = pu16_prom[7] & 0xFU;
-    pu16_prom[7] &= 0xFF00U;
+    u8_crc = (uint8_t)(u16_prom[7] & 0x000FU);
+    u16_prom[7] &= 0xFF00U;
 
     /* if eeprom is all zeros, we're probably fucked - BUT this will return valid CRC lol */
     for (i32_i = 0; i32_i < 8; i32_i++)
     {
-        if (pu16_prom[i32_i] != 0)
+        if (u16_prom[i32_i] != 0U)
         {
-            u8_zero = 0;
+            u8_zero = 0U;
         }
     }
-    if (u8_zero == 0)
+    if (u8_zero == 0U)
     {
         for (i32_i = 0; i32_i < 16; i32_i++)
         {
-            if (i32_i & 1)
+            if ( ((uint32_t)i32_i & 1U) == 1U)
             {
-                u32_res ^= ((pu16_prom[i32_i >> 1]) & 0x00FF);
+
+                 /* u32_res ^= (uint32_t)((u16_prom[(uint32_t)i32_i >> 1U]) & 0x00FF); */
+                u16_value = u16_prom[(uint32_t)i32_i >> 1U];
+                u16_value =  u16_value & 0x00FFU;
+                u32_res  ^= (uint32_t)u16_value;
             }
             else
             {
-                u32_res ^= (pu16_prom[i32_i >> 1] >> 8);
+                u16_value = (u16_prom[(uint32_t)i32_i >> 1U] >> 8U);
+                u32_res ^= (uint32_t)u16_value;
             }
             for (i32_j = 8; i32_j > 0; i32_j--)
             {
-                if (res & 0x8000)
+                if ( (u32_res & 0x8000U) != 0U )
                 {
-                    u32_res ^= 0x1800;
+                    u32_res ^= 0x1800U;
                 }
-                u32_res <<= 1;
+                u32_res <<= 1U;
             }
         }
-        pu16_prom[7] |= u8_crc;
-        if (u8_crc == ((u32_res >> 12) & 0xF))
+        u16_prom[7] |= u8_crc;
+
+        u8_value = ((uint8_t)(u32_res >> 12U) & 0xFU);
+
+        if (u8_crc == u8_value)
         {
             be_result = BOARD_ERR_OK;
         }
@@ -135,16 +155,31 @@ static BOARD_ERROR ms5611_crc(uint16_t *pu16_prom)
 
 static uint32_t ms5611_read_adc(void)
 {
+    BOARD_ERROR be_result = BOARD_ERR_OK;
     uint8_t u8_rxbuf[3];
-    /* i2cRead(MS5611_ADDR, CMD_ADC_READ, 3, rxbuf); /* read ADC */
+    uint32_t u32_value[2];
+
+    /* i2cRead(MS5611_ADDR, CMD_ADC_READ, 3, rxbuf);  read ADC */
     be_result = board_i2c_read( MS5611_ADDR, CMD_ADC_READ, 3U, u8_rxbuf);
-    return ((rxbuf[0] << 16U) | (rxbuf[1] << 8U) | rxbuf[2]);
+
+    /* ((rxbuf[0] << 16U) | (rxbuf[1] << 8U) | rxbuf[2]); */
+
+    u32_value[0] = (uint32_t)u8_rxbuf[0];
+    u32_value[0] = u32_value[0] << 16U;
+    u32_value[1] = (uint32_t)u8_rxbuf[1];
+    u32_value[1] = u32_value[1] << 8U;
+    u32_value[0] = u32_value[0] | u32_value[1];
+    u32_value[1] = (uint32_t)u8_rxbuf[2];
+    u32_value[0] = u32_value[0] | u32_value[1];
+
+    return (u32_value[0]);
 }
 
 static void ms5611_start_ut(void)
 {
-    /* i2cWrite(MS5611_ADDR, CMD_ADC_CONV + CMD_ADC_D2 + ms5611_osr, 1); /* D2 (temperature) conversion start! */
-    be_result = board_i2c_write(MS5611_ADDR, CMD_ADC_CONV + CMD_ADC_D2 + ms5611_osr, 1);
+    BOARD_ERROR be_result = BOARD_ERR_OK;
+    /* i2cWrite(MS5611_ADDR, CMD_ADC_CONV + CMD_ADC_D2 + ms5611_osr, 1);  D2 (temperature) conversion start! */
+    be_result = board_i2c_write(MS5611_ADDR, CMD_ADC_CONV + CMD_ADC_D2 + ms5611_osr, 1U);
 }
 
 static void ms5611_get_ut(void)
@@ -154,8 +189,9 @@ static void ms5611_get_ut(void)
 
 static void ms5611_start_up(void)
 {
-    /* i2cWrite(MS5611_ADDR, CMD_ADC_CONV + CMD_ADC_D1 + ms5611_osr, 1); /* D1 (pressure) conversion start! */
-    be_result = board_i2c_write(MS5611_ADDR, CMD_ADC_CONV + CMD_ADC_D1 + ms5611_osr, 1);
+    BOARD_ERROR be_result = BOARD_ERR_OK;
+    /* i2cWrite(MS5611_ADDR, CMD_ADC_CONV + CMD_ADC_D1 + ms5611_osr, 1);  D1 (pressure) conversion start! */
+    be_result = board_i2c_write(MS5611_ADDR, CMD_ADC_CONV + CMD_ADC_D1 + ms5611_osr, 1U);
 }
 
 
@@ -167,37 +203,46 @@ static void ms5611_get_up(void)
 
 static void ms5611_calculate(int32_t *pressure, int32_t *temperature)
 {
-    uint32_t press;
-    int64_t temp;
-    int64_t delt;
-    int32_t dT = (int64_t)ms5611_ut - ((uint64_t)ms5611_c[5] * 256);
-    int64_t off = ((int64_t)ms5611_c[2] << 16) + (((int64_t)ms5611_c[4] * dT) >> 7);
-    int64_t sens = ((int64_t)ms5611_c[1] << 15) + (((int64_t)ms5611_c[3] * dT) >> 8);
-    temp = 2000 + ((dT * (int64_t)ms5611_c[6]) >> 23);
+    uint32_t u32_press;
+    int64_t i64_temp;
+    int64_t i64_delt;
+    int64_t i64_dT;
+    int64_t i64_off;
+    int64_t sens;
 
-    if (temp < 2000)
+    /* i32_dT = (int64_t)ms5611_ut - (int64_t)((uint64_t)ms5611_c[5] * 256); */
+    i64_dT = (int64_t)ms5611_c[5] * 256;
+    i64_dT = (int64_t)ms5611_ut - i64_dT;
+
+
+    i64_off = ((int64_t)ms5611_c[2] << 16) + (((int64_t)ms5611_c[4] * i32_dT) >> 7);
+    i64_sens = ((int64_t)ms5611_c[1] << 15) + (((int64_t)ms5611_c[3] * i32_dT) >> 8);
+    i64_temp = 2000 + ((i32_dT * (int64_t)ms5611_c[6]) >> 23);
+
+
+    if (i64_temp < 2000)
     { /* temperature lower than 20degC */
-        delt = temp - 2000;
-        delt = 5 * delt * delt;
-        off -= delt >> 1;
-        sens -= delt >> 2;
-        if (temp < -1500)
+        i64_delt = i64_temp - 2000;
+        i64_delt = 5 * i64_delt * i64_delt;
+        i64_off -= i64_delt >> 1;
+        i64_sens -= i64_delt >> 2;
+        if (i64_temp < -1500)
         { /* temperature lower than -15degC */
-            delt = temp + 1500;
-            delt = delt * delt;
-            off  -= 7 * delt;
-            sens -= (11 * delt) >> 1;
+            i64_delt = i64_temp + 1500;
+            i64_delt = i64_delt * i64_delt;
+            i64_off  -= 7 * i64_delt;
+            sens -= (11 * i64_delt) >> 1;
         }
     }
 
-    press = ((((int64_t)ms5611_up * sens ) >> 21) - off) >> 15;
+    u32_press = ((((int64_t)ms5611_up * sens ) >> 21) - i64_off) >> 15;
 
     if (pressure)
     {
-        *pressure = press;
+        *pressure = u32_press;
     }
     if (temperature)
     {
-        *temperature = temp;
+        *temperature = i64_temp;
     }
 }
